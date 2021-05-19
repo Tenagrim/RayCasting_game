7ef2f67ffa624caf4b2001a68ba3cd0d4e7a7eb8
Tenagrim <tenagrim@gmail.com>
2021-05-19 17:36:49 +0300

    renaming

---
+++ game/run_game.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "CONTROLS: =========================="
+echo ""
+echo "WASD:                 move"
+echo "left / right arrows:  rotate"
+echo "M:                    toggle minimap"
+echo "C:                    toggle noclip"
+echo "ESC:                  exit"
+echo ""
+echo "===================================="
+
+make && ./game maps/1.cub
+
